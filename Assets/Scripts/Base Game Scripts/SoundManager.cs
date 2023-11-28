ea9a55eac079d05d1bf687f931683a1b1a2d8d90
hhd <duonglv123177@gmail.com>
2023-11-28 08:09:41 +0700

    khoi tao

---
+++ Assets/Scripts/Base Game Scripts/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	public AudioSource[] destroyNoise;
+
+    public void PlayRandomDestroyNoise()
+	{
+        if (PlayerPrefs.HasKey("Sound"))
+        {
+            if (PlayerPrefs.GetInt("Sound") == 1)
+            {
+                //Choose a random number
+                int clipToPlay = Random.Range(0, destroyNoise.Length);
+                //play that clip
+                destroyNoise[clipToPlay].Play();
+            }
+        }else{
+            //Choose a random number
+            int clipToPlay = Random.Range(0, destroyNoise.Length);
+            //play that clip
+            destroyNoise[clipToPlay].Play();
+        }
+	}
+}
